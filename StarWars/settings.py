9db200838b8269c94018801447284854f39d51a4
Shwetabh Kumar <shwetabh002@gmail.com>
2018-09-03 14:15:08 +0530

    CORS

--- StarWars/settings.py
+++ StarWars/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_cron',
+    'corsheaders',
     'Home',
     'Movies',
     'Planets'
@@ -53,6 +54,7 @@ CRON_CLASSES = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
